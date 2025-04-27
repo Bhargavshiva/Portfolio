f85f3525ba82331cb39925f6a85e9cc61be3aa11
venkatesh <venky@example.com>
2025-04-26 19:54:56 -0700

    commiting new changes

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite config file
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
